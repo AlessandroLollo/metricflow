e84fe53fdccf300ecbe04a73bd201bafb081eb06
Paul Yang <paul@transformdata.io>
2022-04-30 23:30:35 -0700

    Add test helper for the plot time dimension.

--- metricflow/test/time/plot_time_dimension.py
+++ metricflow/test/time/plot_time_dimension.py
@@ -1,3 +1,6 @@
 from metricflow.dataset.dataset import DataSet
+from metricflow.time.time_granularity import TimeGranularity
 
 PTD = DataSet.plot_time_dimension_name()
+PTD_SPEC_DAY = DataSet.plot_time_dimension_spec(TimeGranularity.DAY)
+PTD_SPEC_MONTH = DataSet.plot_time_dimension_spec(TimeGranularity.DAY)
