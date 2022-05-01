86d14e1fdd59e890ff456fa3cea571026484e80b
Paul Yang <paul@transformdata.io>
2022-04-30 23:46:16 -0700

    Use SourceNodeBuilder to provide inputs to DataflowPlanBuilder.

--- metricflow/dataflow/builder/source_node.py
+++ metricflow/dataflow/builder/source_node.py
@@ -16,14 +16,19 @@ class SourceNodeBuilder:
         source_nodes = []
         for data_set in data_sets:
             read_node = ReadSqlSourceNode[DataSourceDataSet](data_set)
-            for time_dimension_reference in self._semantic_model.data_source_semantics.get_aggregation_time_dimensions(
+            aggregation_time_dimensions = self._semantic_model.data_source_semantics.get_aggregation_time_dimensions(
                 data_set.data_source_reference
-            ):
-                source_nodes.append(
-                    PlotTimeDimensionTransformNode[DataSourceDataSet](
-                        parent_node=read_node,
-                        aggregation_time_dimension_reference=time_dimension_reference,
+            )
+            # Dimension sources may not have any measures -> no aggregation time dimensions.
+            if len(aggregation_time_dimensions) == 0:
+                source_nodes.append(read_node)
+            else:
+                for time_dimension_reference in aggregation_time_dimensions:
+                    source_nodes.append(
+                        PlotTimeDimensionTransformNode[DataSourceDataSet](
+                            parent_node=read_node,
+                            aggregation_time_dimension_reference=time_dimension_reference,
+                        )
                     )
-                )
 
         return source_nodes
